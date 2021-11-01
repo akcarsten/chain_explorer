a3296e46b4f387c03d068da8d778db962c3635fd
akcarsten <carsten.a.klein@gmail.com>
2021-11-01 11:24:30 +0100

    get_by_hash function and tests added

--- blockexplorer/explorer.py
+++ blockexplorer/explorer.py
@@ -4,4 +4,11 @@ import codecs
 
 def get_by_block(block):
     url = 'https://blockchain.info/block-height/'
+
     return requests.get(url + str(block) + "?format=json").json()['blocks'][0]
+
+
+def get_by_hash(block_hash):
+    url = 'https://blockchain.info/rawblock/'
+
+    return requests.get(url + block_hash).json()
