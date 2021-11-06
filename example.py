dda7876b2b439ec10b8be82f277251d263ae84aa
akcarsten <carsten.a.klein@gmail.com>
2021-11-06 13:36:41 +0100

    decoding the Bitcoin white paper

--- example.py
+++ example.py
@@ -1,4 +1,5 @@
 import blockexplorer.explorer as exp
+import codecs
 
 # Example 1: Genesis block
 # Encoded in the input message
@@ -25,3 +26,25 @@ raw_block = exp.get_by_block(666666)
 in_msg, out_msg = exp.collect_messages(raw_block)
 msg = exp.decode_hex_message(out_msg)
 print([s for s in msg if 'not' in str(s)])
+
+
+# Example 3: The Bitcoin white paper
+raw_tx = exp.get_transaction('54e48e5f5c656b26c3bca14a8c95aa583d07ebe84dde3b7dd4a78f4e4186e713')
+
+pdf = ''
+n = 0
+for output in raw_tx['out']:
+    if n < 948:
+        cur = 4
+        pdf = pdf + output['script'][cur:cur + 130]
+        cur += 132
+        pdf = pdf + output['script'][cur:cur + 130]
+        cur += 132
+        pdf = pdf + output['script'][cur:cur + 130]
+    n +=1
+
+codecs.decode(pdf[20:-120], 'hex')
+
+f = open("bitcoin.pdf", "wb")
+f.write(codecs.decode(pdf[16:-112], 'hex'))
+f.close()
