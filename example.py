b3c07b4896026cdae60c948021c166c07492dbdf
akcarsten <carsten.a.klein@gmail.com>
2021-11-07 21:36:33 +0100

    Nelson Mandela Wikipedia example added

--- example.py
+++ example.py
@@ -32,9 +32,27 @@ exp.download_data('54e48e5f5c656b26c3bca14a8c95aa583d07ebe84dde3b7dd4a78f4e4186e
 
 
 # Example 4: Satoshis Upload/Download Tool
+# `In a cute touch, these transactions both donate 0.00000001 bitcoins to address 1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa,
+# which is Satoshi Nakamoto's address from the Genesis Block.`
+# -- http://www.righto.com/2014/02/ascii-bernanke-wikileaks-photographs.html#ref13
 
 # The Downloader
 exp.download_data('6c53cd987119ef797d5adccd76241247988a0a5ef783572a9972e7371c5fb0cc', 'downloader.py')
 
 # The Uploader
 exp.download_data('4b72a223007eab8a951d43edc171befeabc7b5dca4213770c88e09ba5b936e17', 'uploader.py')
+
+
+# Example 5: Nelson Mandela
+
+# Part 1: The Wikipedia text
+raw_tx = exp.get_transaction('8881a937a437ff6ce83be3a89d77ea88ee12315f37f7ef0dd3742c30eef92dba')
+
+data = ''
+for output in raw_tx['out']:
+    if output['value'] == 5500:
+        data = data + output['script'][6:46]
+
+print('\nThis is text from the Wikipedia page about Nelson Mandela, encoded on the Bitcoin blockchain:\n')
+print(exp.decode_hex_message(data)[0][8:-114].
+      decode('utf8', errors="ignore"))
